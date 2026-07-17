4ef79a66084e0784e180b30c09e967426662043a
mrhope1884 <vuvietquan1884@gmail.com>
2026-07-18 02:21:08 +0700

    Thêm thư mục routes và cập nhật index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const route = require('./routes/client/index.route');
 const mongoose = require('mongoose');
 
 const app = express()
@@ -7,16 +8,8 @@ const port = 3000;
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-    
-    res.render("client/pages/home/index")
-})
-
-app.get('/Products', async (req, res) => {
-    const products = await Product.find({});
-    console.log(products)
-    res.render("client/pages/products/index")
-})
+//routes
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
